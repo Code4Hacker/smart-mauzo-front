531d7a316403673f2c8d5de64a6a8611d083f234
Code4Hacker <black2022mail@gmail.com>
2023-10-26 21:23:46 +0300

    END

--- src/components/Welcome/ELogin.jsx
+++ src/components/Welcome/ELogin.jsx
@@ -72,7 +72,25 @@ const ELogin = () => {
             handlesubmit();
         }
     }
+    const query_2 = () => {
+        jQuery("button").on("click", function () {
+            jQuery("button *").removeClass("btn_cli");
+            jQuery(this).find(".bn1").hide();
+            jQuery(this).find(".bn2").addClass("btn_cli");
+            jQuery(this).prop("disabled", true);
+            setTimeout(() => {
+                jQuery(this).prop("disabled", false);
+                jQuery(this).find(".bn1").show({
+                    done: function () {
+                        jQuery("button *").removeClass("btn_cli");
+                        jQuery(this).prop("disabled", false);
+                    }
+                });
+            }, 3000);
+        });
+    }
     useEffect(() => {
+        query_2();
         if (window.localStorage.emMail !== undefined) {
             navigate('/employee');
         }
--- src/components/Welcome/Login.jsx
+++ src/components/Welcome/Login.jsx
@@ -69,7 +69,25 @@ const Login = () => {
             handlesubmit();
         }
     }
+    const query_2 = () => {
+        jQuery("button").on("click", function () {
+            jQuery("button *").removeClass("btn_cli");
+            jQuery(this).find(".bn1").hide();
+            jQuery(this).find(".bn2").addClass("btn_cli");
+            jQuery(this).prop("disabled", true);
+            setTimeout(() => {
+                jQuery(this).prop("disabled", false);
+                jQuery(this).find(".bn1").show({
+                    done: function () {
+                        jQuery("button *").removeClass("btn_cli");
+                        jQuery(this).prop("disabled", false);
+                    }
+                });
+            }, 3000);
+        });
+    }
     useEffect(() => {
+        query_2();
         if (window.localStorage.adminmail !== undefined) {
             navigate('/admin');
         }
