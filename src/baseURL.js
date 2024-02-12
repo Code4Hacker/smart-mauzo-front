2fe2c9491f2297aec05b8a2089cae79125c83644
Code4Hacker <black2022mail@gmail.com>
2024-02-12 15:39:56 +0300

    new Land

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost/tail-back/api/v1/";
-export const baseURL2 = "http://localhost/tail-back/api/v1";
-// export const baseURL2 = "https://tailorgemini.000webhostapp.com/tailorwebapp";
-// export const baseURL = "https://tailorgemini.000webhostapp.com/tailorwebapp/";
+// export const baseURL = "http://localhost/tail-back/api/v1/";
+// export const baseURL2 = "http://localhost/tail-back/api/v1";
+export const baseURL2 = "https://tailorgemini.000webhostapp.com/tailorwebapp";
+export const baseURL = "https://tailorgemini.000webhostapp.com/tailorwebapp/";
